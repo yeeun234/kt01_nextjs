f15c5b0bc04a35045010fd60007e8336df4caf2e
yeeun234 <wooyeeun2@gmail.com>
2025-04-24 10:14:47 +0900

    feat: add ProductDetailPage2 with query string handling via searchParams prop
    display product detail by extracting id from searchParams in ProductDetailPage2

--- src/app/productDetail2/page.tsx
+++ src/app/productDetail2/page.tsx
@@ -1,12 +1,28 @@
 import { Product ,products } from '@/types/product'
 import Link from 'next/link';
-export default async function ProductDetailPage2() {
-
+export default async function ProductDetailPage2({
+  searchParams
+} : {
+  searchParams : Promise<{id:string}>
+}) {
+  const {id} = await searchParams ;
+  console.log(id)
+  const item:Product | undefined = products.find((item:Product)=>item.id === id);
+  
+  if(!item){
+    return <div className='mt-5 bg-amber-200'>해당아이템이 없습니다.</div>
+  }
   return (
     <div className='mt-20 bg-amber-50'>
-       
-        <div className='mt-5 bg-amber-200'>
-        <Link href="/productList2">
+       <ol>
+            <li>id : {item.id}  </li>
+            <li>name : {item.name}  </li>
+            <li>category : {item.category}  </li>
+            <li>price : {item.price}  </li>
+            <li>description : {item.description}  </li>
+        </ol>   
+    <div className='mt-5 bg-amber-200'>
+        <Link href="/productList3">
             [이전]
         </Link>
         </div>   
